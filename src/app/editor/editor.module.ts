69f5f70660c3393182af0aad9f61cab6bad94cfa
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2017-12-11 15:36:59 +0100

    start minimal standoff editor

--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { EditorComponent } from './editor/editor.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [EditorComponent],
   exports: [EditorComponent]
--- src/app/editor/editor/editor.component.ts
+++ src/app/editor/editor/editor.component.ts
@@ -7,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
 
+  mode: string = 'enter';
   utf8string: string;
+  selectedString: string;
+  startIndex: number;
+  endIndex: number;
+  formattedText: string;
   constructor() { }
 
   ngOnInit() {
   }
 
+  getSelectionText() {
+    let text = '';
+    let start = 0;
+    let length = 0;
+    if (window.getSelection) {
+      text = window.getSelection().toString();
+      start = window.getSelection().anchorOffset;
+      length = text.length;
+    }
+    if (this.utf8string.substring(start, start + length) === text) {
+      this.selectedString = text;
+      this.startIndex = start;
+      this.endIndex = start + length;
+
+      this.formatText();
+    }
+
+    alert('New standoff annotation <<' + this.selectedString + '>>, from index ' + this.startIndex + ' to ' + this.endIndex);
+  }
+
+  formatText() {
+    this.formattedText = this.utf8string.substring(0, this.startIndex)
+      + '<b>'
+      + this.utf8string.substring(this.startIndex, this.endIndex)
+      + '</b>'
+      + this.utf8string.substring(this.endIndex, this.utf8string.length);
+  }
+
+  exitEditMode() {
+    this.mode = 'tag';
+    this.formattedText = this.utf8string;
+  }
 }
